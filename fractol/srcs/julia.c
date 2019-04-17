fe30390d492909c8352e56ccf1c2101cfadb2652
Schuyler Durgan <sdurgan@oa-l6.21-school.ru>
2019-04-17 18:28:04 +0300

    clean

--- fractol/srcs/julia.c
+++ fractol/srcs/julia.c
@@ -6,7 +6,7 @@
 /*   By: sdurgan <sdurgan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/17 13:50:43 by sdurgan           #+#    #+#             */
-/*   Updated: 2019/04/17 17:06:05 by sdurgan          ###   ########.fr       */
+/*   Updated: 2019/04/17 18:27:41 by sdurgan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	make_julia(t_fctl *fractol)
 	int		x;
 	int		y;
 	int		i = 0;
-	int		maxIterations = 1000;
+	int		maxIterations = 1200;
 	int		color ;
 	double	cRe = -0.7;
 	double	cIm = 0.27015;         
@@ -25,13 +25,12 @@ void	make_julia(t_fctl *fractol)
 	double	zoom = 1, moveX = 0, moveY = 0;
 
 	y = 0;
-	ft_putstr("here");
 	while (y < WIN_H)
 	{
 		x = 0;
 		while (x < WIN_W)
 		{
-			/*newRe = 1.5 * (x - WIN_W / 2) / (0.5 * zoom * WIN_W) + moveX;
+			newRe = 1.5 * (x - WIN_W / 2) / (0.5 * zoom * WIN_W) + moveX;
 			newIm = (y - WIN_H / 2) / (0.5 * zoom * WIN_H) + moveY;
 			while (i < maxIterations)
 			{
@@ -46,7 +45,7 @@ void	make_julia(t_fctl *fractol)
 			color = get_color(i % 256, 255, 255 * (i < maxIterations));
 			put_pix_img(fractol, x, y, color);
 			i = 0;
-			printf("%d, %d\n", x, y);*/
+			printf("%d, %d\n", x, y);
 			x++;
 		}
 		y++;
