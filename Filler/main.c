9fec9bda95e87ea6db65ca044ceec321159162ce
valeria <tito_alba@mail.ru>
2019-08-31 15:13:18 +0300

    test

--- Filler/main.c
+++ Filler/main.c
@@ -1,17 +1,20 @@
 #include "filler.h"
-#include "stdio.h"
+#include <stdio.h>
 
 void    init_filler(t_filler *f) 
 {
     int     ret;
     char    *line;
+    int     len;
 
     ret = 1;
     while (ret > 0)
     {
         f->map_x = 0;
         ret = get_next_line(STDIN_FILENO, &line);
-        printf("lalal %s\n", line);
+        len = ft_strlen(line);
+        write(2, line, len);
+        write(2, "\n", 1);
     }
 }
 
